0492f5670f4b95ee33191b4a81ae7b610da3d1af
amrit choudhary <amrit.choudhary@jetsynthesys.com>
2022-08-17 14:44:36 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
 using System;
+using System.Collections;
 
-public class Example {
+public class Example
+{
     public static void Main()
     {
         Console.Write("Hello ");
+    }
+
+    public float Add(int a, int b)
+    {
+        int c = a + b;
+        return (float)c;
+    }
+
+    public float Sub(int a, int b)
+    {
+        int c = a - b;
+        return (float)c;
     }
 }
